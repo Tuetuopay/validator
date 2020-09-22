1630d4498fccd5f60d9ed5e3b638f3ef3fbfeecc
Tuetuopay <tuetuopay@me.com>
2020-09-22 18:25:37 +0200

    types: Add customs validator
    It is similar to the `custom` validator, but rather than returning a
    ValidationError, it returns an array of validation errors.
    
    The main intent of this custom validator is to implement validation on
    vecs of plain or foreign types (e.g. String).

--- validator_derive/src/lib.rs
+++ validator_derive/src/lib.rs
@@ -462,6 +462,12 @@ fn find_validators_for_field(
                                             None => error(lit.span(), "invalid argument for `custom` validator: only strings are allowed"),
                                         };
                                     }
+                                    "customs" => {
+                                        match lit_to_string(lit) {
+                                            Some(s) => validators.push(FieldValidation::new(Validator::Customs(s))),
+                                            None => error(lit.span(), "invalid argument for `custom` validator: only strings are allowed"),
+                                        };
+                                    }
                                     "contains" => {
                                         match lit_to_string(lit) {
                                             Some(s) => validators.push(FieldValidation::new(Validator::Contains(s))),
@@ -515,6 +521,14 @@ fn find_validators_for_field(
                                         validators.push(extract_custom_validation(
                                             rust_ident.clone(),
                                             attr,
+                                            &meta_items,
+                                        ));
+                                    }
+                                    "customs" => {
+                                        validators.push(extract_one_arg_validation(
+                                            "function",
+                                            ident.to_string(),
+                                            rust_ident.clone(),
                                             &meta_items,
                                         ));
                                     }
--- validator_derive/src/validation.rs
+++ validator_derive/src/validation.rs
@@ -369,6 +369,7 @@ pub fn extract_one_arg_validation(
 
     let validator = match validator_name.as_ref() {
         "custom" => Validator::Custom { function: value.unwrap(), argument: Box::new(None) },
+        "customs" => Validator::Customs(value.unwrap()),
         "contains" => Validator::Contains(value.unwrap()),
         "must_match" => Validator::MustMatch(value.unwrap()),
         "regex" => Validator::Regex(value.unwrap()),
